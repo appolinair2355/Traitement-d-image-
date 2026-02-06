29655633d4b31b4e26b871b04864f785129232b8
appolinair2355 <felicitationsbien990@gmail.com>
2026-02-06 14:23:39 +0100

    Change webhook URL in config.py
    Updated the webhook URL for Render in config.py.

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ API_HASH = "a8639172fa8d35dbfd8ea46286d349ab"
 BOT_TOKEN = "8442253971:AAEisYucgZ49Ej2b-mK9_6DhNrqh9WOc_XU"
 ADMIN = 1190237801
 
-# URL du webhook pour Render (à remplacer par ton URL Render)
-WEBHOOK_URL = f"https://VOTRE_URL_RENDER/{BOT_TOKEN}"
+# URL du webhook pour Render
+WEBHOOK_URL = "https://traitement-d-image.onrender.com"
